6734e8c82c4b5f649dfd4e7030c217cdd4b062fe
tac <tac@tacnoMacBook-Air.local>
2025-10-05 19:50:35 +0900

    testコマンドを削除

--- srcs/CommandHandler.cpp
+++ srcs/CommandHandler.cpp
@@ -33,7 +33,6 @@ void CommandHandler::initCommandMap() {
   commandMap_["INVITE"] = Command::invite;
   commandMap_["MODE"] = Command::mode;
   commandMap_["TOPIC"] = Command::topic;
-  commandMap_["DEBUG"] = Command::debug;
   commandMap_["MOTD"] = Command::motd;
   commandMap_["PRIVMSG"] = Command::privmsg;
   commandMap_["NOTICE"] = Command::notice;
